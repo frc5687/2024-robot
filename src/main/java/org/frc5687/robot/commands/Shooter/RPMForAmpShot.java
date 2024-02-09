39ef0f180eab42efe311934e2031fd03a8025335
Hailstorm8441 <145611502+Hailstorm8441@users.noreply.github.com>
2024-02-09 13:45:56 -0500

    Made RPMForAmpShot have addrequirements()
    still untested :3

--- src/main/java/org/frc5687/robot/commands/Shooter/RPMForAmpShot.java
+++ src/main/java/org/frc5687/robot/commands/Shooter/RPMForAmpShot.java
@@ -10,6 +10,7 @@ public class RPMForAmpShot extends OutliersCommand{
     
     public RPMForAmpShot(Shooter shooter) {
         _shooter = shooter;
+        addRequirements(_shooter);
     }
 
     @Override
