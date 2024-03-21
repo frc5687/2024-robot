a74692b2ed3bb6f5868808b9e9f1f7270ff4201b
freyjaprescott <155899136+freyjaprescott@users.noreply.github.com>
2024-03-21 17:11:08 -0400

    Slow Mode fixes

---
+++ src/main/java/org/frc5687/robot/commands/DriveTrain/SlowMode.java
@@ -0,0 +1,38 @@
+package org.frc5687.robot.commands.DriveTrain;
+
+import static org.frc5687.robot.Constants.DriveTrain.SLOW_KINEMATIC_LIMITS;
+
+import org.frc5687.robot.commands.OutliersCommand;
+import org.frc5687.robot.subsystems.DriveTrain;
+
+public class SlowMode extends OutliersCommand {
+        private DriveTrain _driveTrain;
+    
+    public SlowMode(DriveTrain drivetrain) {
+        _driveTrain = drivetrain;
+    }
+
+    @Override
+    public void initialize() {
+        _driveTrain.disableAutoShifter();
+    }
+
+    @Override
+    public void execute() {
+        _driveTrain.setKinematicLimits(SLOW_KINEMATIC_LIMITS);
+        if (!_driveTrain.isLowGear()) {
+            _driveTrain.shiftDownModules();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        _driveTrain.enableAutoShifter();
+    }
+}
